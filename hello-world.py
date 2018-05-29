4ecf91efb5dbc13679431b5256bc9d02ca56d6ef
JohJohOneThousand <think@haulpaq.com>
2018-05-29 21:53:35 +1000

    as

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,10 @@
-# comments go here
-x = 345
-y = 87
-print(x * y)
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+s = "MIT 6.0001 Introduction"
+print(s)
+l=list(s)
+print(l)
