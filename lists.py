5518d5f30012adfa534fdc3b1a71dcd60a080da4
JohJohOneThousand <think@haulpaq.com>
2018-06-05 21:26:54 +1000

    fg

---
+++ lists.py
@@ -0,0 +1,18 @@
+# lists
+
+alf_team_names = ['Crows', 'Lions', 'Blues', 'Magpies', 'Bombers', 'Dockers', 'Cats', 'Suns', 'Giants', 'Hawks', 'Demons', 'Kangaroos', 'Power', 'Tigers', 'Saints', 'Swans', 'Eagles', 'Bulldogs']
+
+alf_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football Club', 'Collingwood Football Club', 
+'Essendon Football Club', 'Fremantle Football Club', 'Geelong Football Club', 'Gold Coast Football Club', 'Greater Western Sydney Giants', 'Hawthorn Football Club', 'Melbourne Football Club', 'North Melbourne Football Club', 'Port Adelaide Football Club', 'Richmond Football Club', 'St Kilda Football Club', 'Sydney Swans', 'West Coast Eagles', 'Western Bulldogs']
+
+# for team in alf_teams
+#     print(team)
+
+# This is Slicing
+# print(alf_team_names[8:])
+# print(alf_club_names)
+
+# print(alf_club_names.extend(alf_team_names))
+
+# alf_club_names.extend(alf_team_names)
+print(alf_club_names.extend(alf_team_names))
