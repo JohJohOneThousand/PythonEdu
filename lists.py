348aa21b6d0f914f87219d14cc2e768a02c42bf2
JohJohOneThousand <think@haulpaq.com>
2018-06-06 07:45:25 +1000

    f

--- lists.py
+++ lists.py
@@ -1,18 +1,20 @@
 # lists
 
-alf_team_names = ['Crows', 'Lions', 'Blues', 'Magpies', 'Bombers', 'Dockers', 'Cats', 'Suns', 'Giants', 'Hawks', 'Demons', 'Kangaroos', 'Power', 'Tigers', 'Saints', 'Swans', 'Eagles', 'Bulldogs']
+afl_team_names = ['Crows', 'Lions', 'Blues', 'Magpies', 'Bombers', 'Dockers', 'Cats', 'Suns', 'Giants', 'Hawks', 'Demons', 'Kangaroos', 'Power', 'Tigers', 'Saints', 'Swans', 'Eagles', 'Bulldogs']
 
-alf_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football Club', 'Collingwood Football Club', 
+afl_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football Club', 'Collingwood Football Club', 
 'Essendon Football Club', 'Fremantle Football Club', 'Geelong Football Club', 'Gold Coast Football Club', 'Greater Western Sydney Giants', 'Hawthorn Football Club', 'Melbourne Football Club', 'North Melbourne Football Club', 'Port Adelaide Football Club', 'Richmond Football Club', 'St Kilda Football Club', 'Sydney Swans', 'West Coast Eagles', 'Western Bulldogs']
 
-# for team in alf_teams
+# for team in afl_teams
 #     print(team)
 
 # This is Slicing
-# print(alf_team_names[8:])
-# print(alf_club_names)
+# print(afl_team_names[8:])
+# print(afl_club_names)
 
-# print(alf_club_names.extend(alf_team_names))
+# print(afl_club_names.extend(afl_team_names))
 
-# alf_club_names.extend(alf_team_names)
-print(alf_club_names.extend(alf_team_names))
+# afl_club_names.extend(afl_team_names)
+# print(str(afl_club_names.extend(afl_team_names)))
+afl_club_names.reverse()
+print(afl_club_names)
