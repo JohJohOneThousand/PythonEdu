556492e01412809172244da7d2e9dfdc033a946f
JohJohOneThousand <think@haulpaq.com>
2018-06-07 22:20:25 +1000

    j

--- lists.py
+++ lists.py
@@ -1,9 +1,10 @@
 # lists
 
-afl_team_names = ['Crows', 'Lions', 'Blues', 'Magpies', 'Bombers', 'Dockers', 'Cats', 'Suns', 'Giants', 'Hawks', 'Demons', 'Kangaroos', 'Power', 'Tigers', 'Saints', 'Swans', 'Eagles', 'Bulldogs']
+afl_team_names = ['Crows', 'Lions', 'Blues', 'Magpies', 'Bombers', 'Dockers', 'Cats', 'Suns',
+                  'Giants', 'Hawks', 'Demons', 'Kangaroos', 'Power', 'Tigers', 'Saints', 'Swans', 'Eagles', 'Bulldogs']
 
-afl_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football Club', 'Collingwood Football Club', 
-'Essendon Football Club', 'Fremantle Football Club', 'Geelong Football Club', 'Gold Coast Football Club', 'Greater Western Sydney Giants', 'Hawthorn Football Club', 'Melbourne Football Club', 'North Melbourne Football Club', 'Port Adelaide Football Club', 'Richmond Football Club', 'St Kilda Football Club', 'Sydney Swans', 'West Coast Eagles', 'Western Bulldogs']
+afl_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football Club', 'Collingwood Football Club',
+                  'Essendon Football Club', 'Fremantle Football Club', 'Geelong Football Club', 'Gold Coast Football Club', 'Greater Western Sydney Giants', 'Hawthorn Football Club', 'Melbourne Football Club', 'North Melbourne Football Club', 'Port Adelaide Football Club', 'Richmond Football Club', 'St Kilda Football Club', 'Sydney Swans', 'West Coast Eagles', 'Western Bulldogs']
 
 # for team in afl_teams
 #     print(team)
@@ -12,7 +13,7 @@ afl_club_names = ['Adelaide Football Club', 'Brisbane Lions', 'Carlton Football 
 # print(afl_team_names[8:])
 # print(afl_club_names)
 
-afl_club_names.extend(afl_team_names)
+afl_club_names.extend(afl_team_names, 'joh')
 
 
 print(afl_club_names)
